cfb81f7084955f5074bd6f8585166b0bf3a793c4
Sanskar <sanskartakkamore@gmail.com>
2022-07-24 12:46:16 +0530

    Working on insert many

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
 import mongoose from 'mongoose';
+//var express = require('express');
+//var router = express.Router();
 const mongoURI = "mongodb://localhost:27017/Nasa-cached-db"
 mongoose.connect(mongoURI, () => {
     console.log("Connected to Mongo Successfully");
@@ -33,50 +35,52 @@ const cacheSchema= new mongoose.Schema({
       }
 
 });
-// const connectToMongo = require('./db');
-// const express = require('express')
-// connectToMongo();
-// const app = express()
-// const port = 3000
-// app.get('/', (req, res) => {
-//     res.send('Hello Rover')
-// })
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-// })
+
 
 
 const Post = mongoose.model('Post',cacheSchema );
-async function getPost() {
-   const nasa = await fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0&api_key=DEMO_KEY");
+async function getPost(req,res) {
+   const nasa = await fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=DEMO_KEY");
    const response = await nasa.json();
    //for(let i=0; i<=response.photos.length;i++){
        //console.log(response[i]['camera']);
+       //console.log(response.photos[0]);
        const newPhotos = [];
-response.photos.forEach(p=>{
-    if(p.id){
-        newPhotos.push({id: p.id,
+       response.photos.forEach(p=>{
+            if(p.id){
+                newPhotos.push({id: p.id,
                 camera: p.camera.name,
                 roverName: p.rover.name,
                 sol: p.sol,
                 date: p.date
-        });
-    }    
-});
-//        const i=0;
-//        console.log(response.photos[0]);
-//    const post = new Post ({
-//         id:response.photos[i]['id'],
-//         sol:response.photos[i]['sol'],
-//         camera:response.photos[i]['camera'].name,
-//         img_src:response.photos[i]['img_src'],
-//         rover:response.photos[i]['rover'].name,
+                });
+            }    
+    });
+    console.log(newPhotos);
+    // console.log('do code is working till here') 
+    // response.newPhotos.insertMany([ response.photos
+    
+    //  ])
 
+    cacheSchema.insertMany(newPhotos, function(error, docs) {});
+    // try {
+    //     const saveData = await newPhotos.save()
+    //     res.json(saveData)
+    //     console.log("is working")
+    // } catch(err) {
+    //     res.json({
+    //         message: err
+    //     })
+    //  }
+     console.log("is it working till here")
+    // response.collection("posts").insertMany(newPhotos, (error, res) => {
+    //     if (error) throw error;
+    
+    //     console.log('Docs inserted!');
+    //     db.close();
+    //   });
 
-//     });
-   //console.log(post)
-    Post.;
-   }
-//}
+}
+
 
 getPost();
